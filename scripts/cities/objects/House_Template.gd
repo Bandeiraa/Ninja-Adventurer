1d8803790386d201c72de84a61c6d7e2b48a98e8
Davi <fcodavibandeira@edu.unifor.br>
2021-06-16 23:10:01 -0300

    Level Design and Player Input

---
+++ scripts/cities/objects/House_Template.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+class_name House
+
+export(bool) var can_enter
+
+func on_body_entered(_body: Player) -> void:
+	pass # Replace with function body.
+
+
+func on_body_exited(_body: Player) -> void:
+	pass # Replace with function body.
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,7 +2,32 @@ extends KinematicBody2D
 class_name Player
 
 onready var sprite: Sprite = get_node("Sprite")
+onready var animation_tree: AnimationTree = get_node("AnimationTree")
+onready var animation = animation_tree.get("parameters/playback")
+
+export(int) var speed
 
 func _ready() -> void:
 	Data.loadData()
 	sprite.texture = load(Data.stored_data.class)
+	
+	
+func _physics_process(_delta: float) -> void:
+	move()
+	
+	
+func move() -> void:
+	var velocity: Vector2 = Vector2.ZERO
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	velocity = velocity.normalized()
+	
+	if velocity != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", velocity)
+		animation_tree.set("parameters/Move/blend_position", velocity)
+		animation.travel("Move")
+		
+	else:
+		animation.travel("Idle")
+	
+	velocity = move_and_slide(velocity * speed)
